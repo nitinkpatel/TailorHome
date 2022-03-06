df864c775c8ab80c18d7bef45b50221a236ef71c
Nitin Kumar Patel <niit.patel@gmail.com>
2022-03-06 15:19:17 -0600

    feature

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
+import styles from "../styles/Footer.module.css";
 
 const Footer = () => {
   return (
-    <div>
-      footer
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <Image
+          src="/img/tailorhomelogo(1).png"
+          objectFit="cover"
+          layout="fill"
+          alt=""
+        />
+      </div>
+      <div className={styles.item}>
+        <div className={styles.card}>
+          <h2 className={styles.motto}>
+            Indian Ethnic Dress Collection
+          </h2>
+        </div>
     </div>
-  )
-}
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,17 +1,37 @@
-import React from 'react'
+import React from "react";
 import Image from "next/image";
-
-// import styles from "../styles/Navbar.module.css";
-
+import styles from "../styles/Navbar.module.css";
 
 const Navbar = () => {
   return (
-    <div> 
-    <div>
-        <Image src="/img/tailorhomemsg.png" alt="" width="64" height="64" />
-      </div>  
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <div className={styles.button}>
+          <Image src="/img/tailorhomemsg.png" alt="" width="64" height="64" />
+        </div>
+        <div className={styles.texts}>
+          <div className={styles.text}>Book Appointment!</div>
+          <div className={styles.text}>204 298 8220</div>
+        </div>
       </div>
-  )
-}
 
-export default Navbar
+     
+
+      <div className={styles.item}>
+        <ul className={styles.list}>
+        <Image
+        src="/img/tailorhomelogo(3).png"
+        alt=""
+        width="16px"
+        height="69px"
+      />
+          <li className={styles.listItem}>Home</li>
+          <li className={styles.listItem}>Collection</li>
+          <li className={styles.listItem}>Contact Us</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
